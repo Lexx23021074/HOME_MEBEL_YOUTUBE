db68cf31c799b896ba3eac2000169d5d0a287696
lexx2 <lexx23021074@gmail.com>
2025-09-06 20:33:34 +0300

    Register simple tag for categories

--- app/urls.py
+++ app/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from app.settings import DEBUG
+from django.conf.urls.static import static
+
+from app import settings
 
 from debug_toolbar import urls as debug_toolbar_urls
 
@@ -33,6 +35,7 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar_urls)),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 '''
 www.site.com/admin/
--- main/views.py
+++ main/views.py
@@ -5,13 +5,10 @@ from goods.models import Categories
 
 def index(request):
 
-    categories = Categories.objects.all()
-
+    
     context : dict[str,Any] = {
         'title' : "Home - Головна",
         'content' : "Магазин меблів HOME",
-        'categories' : categories
-
     }
 
     return render(request, 'main/index.html', context )
