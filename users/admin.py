8e30051ff426a5b1ee4a1550e6e73714b7535c74
lexx2 <lexx23021074@gmail.com>
2025-10-28 22:58:26 +0200

    Create order is ready

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from carts.admin import CartTabAdmin
+from orders.admin import OrderTabulareAdmin
 from users.models import User
 
 # admin.site.register(User)
@@ -12,4 +13,4 @@ class  UserAdmin(admin.ModelAdmin):
 
     
 
-    inlines =[CartTabAdmin,]
+    inlines =[CartTabAdmin, OrderTabulareAdmin]
