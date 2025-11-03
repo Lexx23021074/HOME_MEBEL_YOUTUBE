79c39378cf998e28edd9940b16286adf98c4c88b
lexx2 <lexx23021074@gmail.com>
2025-11-03 16:00:33 +0200

    context variable 'orders' -> 'order' in carts views

--- carts/views.py
+++ carts/views.py
@@ -96,7 +96,7 @@ def cart_remove(request):
 
     referer = request.META.get('HTTP_REFERER')
     if reverse('orders:create_order') in referer:
-        context["orders"] = True
+        context["order"] = True
 
     cart_items_html = render_to_string(
         "carts/includes/included_cart.html", context, request=request)
